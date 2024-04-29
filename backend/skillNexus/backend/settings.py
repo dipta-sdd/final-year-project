3fe8af02734ecced59432352f54b322e4d5ee500
dipta-sdd <dipta5160@gmail.com>
2024-04-29 16:41:50 +0600

    dfgdf

--- backend/skillNexus/backend/settings.py
+++ backend/skillNexus/backend/settings.py
@@ -64,25 +64,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
 }
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            # ADD YOUR DIRECTORY HERE LIKE SO:
-            os.path.join(BASE_DIR, 'templates/')
-            BASE_DIR + '/templates/',
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
